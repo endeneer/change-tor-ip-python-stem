9b967970f5c37b4a26bcd9c8f196aec5b1583451
endeneer <61776978+endeneer@users.noreply.github.com>
2020-05-19 08:50:28 -0400

    Add files via upload

---
+++ tor-renew-ip.py
@@ -0,0 +1,28 @@
+import time
+import socket
+import socks
+
+from urllib.request import urlopen
+from stem import Signal
+from stem.control import Controller
+
+controller = Controller.from_port(port=9051)
+
+def connectTor():
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
+    socket.socket = socks.socksocket
+
+def renewTor():
+    controller.authenticate("abc123")
+    controller.signal(Signal.NEWNYM)
+
+def showIP():
+    print(urlopen('http://icanhazip.com').read())
+
+for i in range(5):
+    renewTor()
+    connectTor()
+    showIP()
+    time.sleep(10)
+
+
